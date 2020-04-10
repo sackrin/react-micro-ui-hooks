346ef9f3073586a40fef8b78f3f7275db90c641a
Hilary Osborne <h.t.osborne@gmail.com>
2020-04-10 13:33:31 +1000

    added license and scripts

--- package-scripts.js
+++ package-scripts.js
@@ -2,17 +2,15 @@ const { series, rimraf, mkdirp } = require('nps-utils');
 
 module.exports = {
   scripts: {
-    default: 'nps production',
+    default: 'nps build',
     clean: {
       description: 'Deletes the various generated folders',
       script: rimraf('./lib'),
     },
-    deploy: {
-      description: 'Builds the assets',
-      default: series('npx nps deploy.build'),
+    build: {
+      description: 'Builds the library assets',
+      default: series('npx nps clean', mkdirp('lib'), `npx babel src --out-dir ./lib`),
       watch: 'nodemon --watch src --exec npx nps deploy',
-      build: series('npx nps clean', 'npx nps deploy.lib'),
-      lib: series(mkdirp('lib'), `npx babel src --out-dir ./lib`),
     },
   },
 };
